1be103ac4ecd76a51956c593326921d8fa21c648
Phyo Kyaw San <phyojupiter9@gmail.com>
2019-09-06 20:05:12 +0630

    CMake build compliant with example build in unix-bin\CMakeCache

---
+++ build/unix-bin/CMakeFiles/cpp_electric_bill_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_electric_bill_calculator.dir/BillCalculateSupport.cpp.obj"
+  "CMakeFiles/cpp_electric_bill_calculator.dir/DomesticBillCalculator.cpp.obj"
+  "CMakeFiles/cpp_electric_bill_calculator.dir/DomesticUnits.cpp.obj"
+  "CMakeFiles/cpp_electric_bill_calculator.dir/IndustrialBillCalculator.cpp.obj"
+  "CMakeFiles/cpp_electric_bill_calculator.dir/IndustrialUnits.cpp.obj"
+  "CMakeFiles/cpp_electric_bill_calculator.dir/main.cpp.obj"
+  "cpp_electric_bill_calculator.exe"
+  "cpp_electric_bill_calculator.exe.manifest"
+  "cpp_electric_bill_calculator.pdb"
+  "libcpp_electric_bill_calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_electric_bill_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
