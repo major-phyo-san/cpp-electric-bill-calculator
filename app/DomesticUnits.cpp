c7e527693783dcdc131a3a4076531041ba856102
Phyo Kyaw San <phyojupiter9@gmail.com>
2022-05-16 23:13:10 +0630

    project refactored

--- app/DomesticUnits.cpp
+++ app/DomesticUnits.cpp
@@ -1,59 +1,67 @@
 //
-// Created by Techman on 22/07/2019.
+// Created by Techman on 23/07/2019.
+// Updated by MajorSan on 19/04/2020.
 //
 
 #include "include/DomesticUnits.h"
 
-double *getDomesticUnitSegments(double inputUnits)
-{
-    if (inputUnits >= 0 && inputUnits <= 30)
+const int LAYER_0_LIMIT = 30;
+const int LAYER_1_LIMIT = 50;
+const int LAYER_2_LIMIT = 75;
+const int LAYER_3_LIMIT = 100;
+const int LAYER_4_LIMIT = 150;
+const int LAYER_5_LIMIT = 200;
+
+double* getDomesticUnitSegments(double inputUnits) {
+    if (inputUnits >= 0 && inputUnits <= LAYER_0_LIMIT)
     {
         unitSegments[0] = inputUnits;
     }
-    else if (inputUnits >= 0 && inputUnits > 30 && inputUnits <= 50)
+    else if (inputUnits >= 0 && inputUnits > LAYER_0_LIMIT && inputUnits <= LAYER_1_LIMIT)
     {
-        unitSegments[0] = 30;
+        unitSegments[0] = LAYER_0_LIMIT; //30
         unitSegments[1] = inputUnits - unitSegments[0];
+
     }
-    else if (inputUnits >= 0 && inputUnits > 50 && inputUnits <= 75)
+    else if (inputUnits >= 0 && inputUnits > LAYER_1_LIMIT && inputUnits <= LAYER_2_LIMIT)
     {
-        unitSegments[0] = 30;
-        unitSegments[1] = 20;
+        unitSegments[0] = LAYER_0_LIMIT; //30
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //50 - 30 => 20
         unitSegments[2] = inputUnits - (unitSegments[0] + unitSegments[1]);
     }
-    else if (inputUnits >= 0 && inputUnits > 75 && inputUnits <= 100)
+    else if (inputUnits >= 0 && inputUnits > LAYER_2_LIMIT && inputUnits <= LAYER_3_LIMIT)
     {
-        unitSegments[0] = 30;
-        unitSegments[1] = 20;
-        unitSegments[2] = 25;
+        unitSegments[0] = LAYER_0_LIMIT; //30
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //50 - 30 => 20
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]); //75 - (30+20) => 25
         unitSegments[3] = inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2]);
     }
-    else if (inputUnits >= 0 && inputUnits > 100 && inputUnits <= 150)
+    else if (inputUnits >= 0 && inputUnits > LAYER_3_LIMIT && inputUnits <= LAYER_4_LIMIT)
     {
-        unitSegments[0] = 30;
-        unitSegments[1] = 20;
-        unitSegments[2] = 25;
-        unitSegments[3] = 25;
+        unitSegments[0] = LAYER_0_LIMIT; //30
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; // 50 - 30 => 20
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]); // 75 - (30+20) => 25
+        unitSegments[3] = LAYER_3_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2]); // 100 - (30+20+25) => 25
         unitSegments[4] = inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3]);
     }
-    else if (inputUnits >= 0 && inputUnits > 150 && inputUnits <= 200)
+    else if (inputUnits >= 0 && inputUnits > LAYER_4_LIMIT && inputUnits <= LAYER_5_LIMIT)
     {
-        unitSegments[0] = 30;
-        unitSegments[1] = 20;
-        unitSegments[2] = 25;
-        unitSegments[3] = 25;
-        unitSegments[4] = 50;
+        unitSegments[0] = LAYER_0_LIMIT; //30
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //20
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]); //25
+        unitSegments[3] = LAYER_3_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2]); //25
+        unitSegments[4] = LAYER_4_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3]); //150 - (30+20+25+25) => 50;
         unitSegments[5] =
-            inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4]);
+                inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4]);
     }
-    else if (inputUnits >= 0 && inputUnits > 200)
+    else if (inputUnits >= 0 && inputUnits > LAYER_5_LIMIT)
     {
-        unitSegments[0] = 30;
-        unitSegments[1] = 20;
-        unitSegments[2] = 25;
-        unitSegments[3] = 25;
-        unitSegments[4] = 50;
-        unitSegments[5] = 50;
+        unitSegments[0] = LAYER_0_LIMIT; //30
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //20
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]); //25
+        unitSegments[3] = LAYER_3_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2]); //25
+        unitSegments[4] = LAYER_4_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3]); //150 - (25+25+20+30) => 50;
+        unitSegments[5] = LAYER_5_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4]); //200 - (30+20+25+25+50) => 50
         unitSegments[6] = inputUnits -
                           (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4] +
                            unitSegments[5]);
--- app/IndustrialUnits.cpp
+++ app/IndustrialUnits.cpp
@@ -1,66 +1,73 @@
 //
-// Created by Techman on 22/07/2019.
+// Created by Techman on 23/07/2019.
+// Updated by MajorSan on 19/04/2020.
 //
 
 #include "include/IndustrialUnits.h"
 
-double *getIndustrialUnitSegments(double inputUnits)
-{
+const int LAYER_0_LIMIT = 500;
+const int LAYER_1_LIMIT = 5000;
+const int LAYER_2_LIMIT = 10000;
+const int LAYER_3_LIMIT = 20000;
+const int LAYER_4_LIMIT = 50000;
+const int LAYER_5_LIMIT = 100000;
 
-    if (inputUnits >= 0 && inputUnits <= 500)
+double* getIndustrialUnitSegments(double inputUnits)
+{
+    if(inputUnits>=0 && inputUnits<=LAYER_0_LIMIT)
     {
         unitSegments[0] = inputUnits;
     }
 
-    else if (inputUnits >= 0 && inputUnits > 500 && inputUnits <= 5000)
+    else if(inputUnits>=0 && inputUnits>LAYER_0_LIMIT && inputUnits<=LAYER_1_LIMIT)
     {
-        unitSegments[0] = 500;
+        unitSegments[0] = LAYER_0_LIMIT;
         unitSegments[1] = inputUnits - unitSegments[0];
     }
 
-    else if (inputUnits >= 0 && inputUnits > 5000 && inputUnits <= 10000)
+    else if(inputUnits>=0 && inputUnits>LAYER_1_LIMIT && inputUnits<=LAYER_2_LIMIT)
     {
-        unitSegments[0] = 500;
-        unitSegments[1] = 4500;
-        unitSegments[2] = inputUnits - (unitSegments[0] + unitSegments[1]);
+        unitSegments[0] = LAYER_0_LIMIT;
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //5000 - 500 => 4500
+        unitSegments[2] = inputUnits - (unitSegments[0]+unitSegments[1]);
     }
 
-    else if (inputUnits >= 0 && inputUnits > 10000 && inputUnits <= 20000)
+    else if(inputUnits>=0 && inputUnits>LAYER_2_LIMIT && inputUnits<=LAYER_3_LIMIT)
     {
-        unitSegments[0] = 500;
-        unitSegments[1] = 4500;
-        unitSegments[2] = 5000;
-        unitSegments[3] = inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2]);
+        unitSegments[0] = LAYER_0_LIMIT;
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //4500
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]); //10000 -(500+4500) => 5000;
+        unitSegments[3] = inputUnits - (unitSegments[0]+unitSegments[1]+unitSegments[2]);
     }
 
-    else if (inputUnits >= 0 && inputUnits > 20000 && inputUnits <= 50000)
+    else if(inputUnits>=0 && inputUnits>LAYER_3_LIMIT && inputUnits<=LAYER_4_LIMIT)
     {
-        unitSegments[0] = 500;
-        unitSegments[1] = 4500;
-        unitSegments[2] = 5000;
-        unitSegments[3] = 10000;
-        unitSegments[4] = inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3]);
+        unitSegments[0] = LAYER_0_LIMIT; //500
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0]; //4500
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]); //5000
+        unitSegments[3] = LAYER_3_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2]); //20000 - (500+4500+5000)
+        unitSegments[4] = inputUnits - (unitSegments[0]+unitSegments[1]+unitSegments[2]+unitSegments[3]);
     }
 
-    else if (inputUnits >= 0 && inputUnits > 50000 && inputUnits <= 100000)
+    else if(inputUnits>=0 && inputUnits>LAYER_4_LIMIT && inputUnits<=LAYER_5_LIMIT)
     {
-        unitSegments[0] = 500;
-        unitSegments[1] = 4500;
-        unitSegments[2] = 5000;
-        unitSegments[3] = 10000;
-        unitSegments[4] = 30000;
-        unitSegments[5] = inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4]);
+        unitSegments[0] = LAYER_0_LIMIT;
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0];
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]);
+        unitSegments[3] = LAYER_3_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[3]);
+        unitSegments[4] = LAYER_4_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3]);
+        unitSegments[5] = inputUnits - (unitSegments[0]+unitSegments[1]+unitSegments[2]+unitSegments[3]+unitSegments[4]);
     }
 
-    else if (inputUnits >= 0 && inputUnits > 100000)
+    else if(inputUnits>=0 && inputUnits>LAYER_5_LIMIT)
     {
-        unitSegments[0] = 500;
-        unitSegments[1] = 4500;
-        unitSegments[2] = 5000;
-        unitSegments[3] = 10000;
-        unitSegments[4] = 30000;
-        unitSegments[5] = 50000;
-        unitSegments[6] = inputUnits - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4] + unitSegments[5]);
+        unitSegments[0] = LAYER_0_LIMIT;
+        unitSegments[1] = LAYER_1_LIMIT - unitSegments[0];
+        unitSegments[2] = LAYER_2_LIMIT - (unitSegments[0] + unitSegments[1]);
+        unitSegments[3] = LAYER_3_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[3]);
+        unitSegments[4] = LAYER_4_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3]);
+        unitSegments[5] = LAYER_5_LIMIT - (unitSegments[0] + unitSegments[1] + unitSegments[2] + unitSegments[3] + unitSegments[4]);
+        unitSegments[6] = inputUnits - (unitSegments[0]+unitSegments[1]+unitSegments[2]+unitSegments[3]+unitSegments[4]+unitSegments[5]);
     }
 
     else
